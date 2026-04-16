21300b5b368f879bd37bec1ff62aade7324a76af
Ashutosh Singh Rana <ashutoshrana3815@gmail.com>
2026-04-16 18:26:40 +0530

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./Frontend/index.html",
+    "./Frontend/src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
